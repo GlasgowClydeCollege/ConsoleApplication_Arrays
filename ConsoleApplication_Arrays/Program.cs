e9bbf09b21bf9e04ec3d27f7cbc6235ef60ed108
daveball <dave.mike.ball@gmail.com>
2017-01-17 12:47:44 +0000

    Added  tasks as  comments

--- ConsoleApplication_Arrays/Program.cs
+++ ConsoleApplication_Arrays/Program.cs
@@ -7,6 +7,19 @@ namespace ConsoleApplication_Arrays
     {
         public static void Main(string[] args)
         {
+
+            /*
+            Create an array of integers containing the values {0,55,4,2,6,7,8,34,65}
+            Create a  method to print out the contents of an array in reverse order
+            Create a method to working out the avaerage value of the contents of the array
+            Create a method to  work out the maz value  in the array
+            Create a method to  work out the min value in the array
+            Create a metod to  work out the sum of the array
+            Read the  following string into an array of strings
+
+            “On the other hand the Nac Mac Feegle were always looking for a fight, in a cheerful sort of way, and when they had no one to fight they fought one another, and if one was all by himself he’d kick his own nose just to keep in practice.”
+            Create a method to reorder the array into alphabetical  order
+*/
         }
     }
 }
